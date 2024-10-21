ca2df3614979e8edae4cb2099b1765e557c39626
gabriella-guimaraes <gabigara13@gmail.com>
2024-10-20 22:00:31 -0300

    feat: plans component - template

--- src/app/Components/Pages/home/home.component.ts
+++ src/app/Components/Pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../Templates/hero/hero.component';
 import { ServicesComponent } from '../../Templates/services/services.component';
+import { PlansComponent } from '../../Templates/plans/plans.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, ServicesComponent],
+  imports: [HeroComponent, ServicesComponent, PlansComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
