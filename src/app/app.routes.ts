8a59fcb53ce34b042247010549020f6537da50e7
gabriella-guimaraes <gabigara13@gmail.com>
2024-10-28 19:45:17 -0300

    feat: app routing and about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/Pages/home/home.component';
+import { AboutComponent } from './Components/Pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: HomeComponent},
+    { path: "about", component: AboutComponent }
+];
